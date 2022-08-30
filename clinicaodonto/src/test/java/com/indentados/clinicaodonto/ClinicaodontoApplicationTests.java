e34cf6c2ff2109b1f77a0b6f4ca5b746aa208cbb
Belicio Cardoso <belloinfo@gmail.com>
2022-08-29 21:45:19 -0300

    Primeiro commit

---
+++ clinicaodonto/src/test/java/com/indentados/clinicaodonto/ClinicaodontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indentados.clinicaodonto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaodontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
