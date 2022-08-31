ca2aea3e4537a435bb04d55f4a3d77c08be1da81
rbsebold <89108421+rbsebold@users.noreply.github.com>
2022-08-31 18:42:24 -0300

    teste

--- clinicaodonto/src/main/java/com/indentados/clinicaodonto/model/Dentista.java
+++ clinicaodonto/src/main/java/com/indentados/clinicaodonto/model/Dentista.java
@@ -1,5 +1,14 @@
 package com.indentados.clinicaodonto.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Dentista extends Pessoa{
     private String matricula;
 }
