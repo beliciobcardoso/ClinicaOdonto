bec0658b15cd53d1bcfb0fd205b97afd8f189ee3
Belicio Cardoso <belloinfo@gmail.com>
2022-08-31 19:21:36 -0300

    Concluindo a classe pessoa

--- clinicaodonto/src/main/java/com/indentados/clinicaodonto/model/Pessoa.java
+++ clinicaodonto/src/main/java/com/indentados/clinicaodonto/model/Pessoa.java
@@ -1,4 +1,21 @@
 package com.indentados.clinicaodonto.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Pessoa {
+    public Integer id;
+    public String nome;
+    public String sobreNome;
+    public Endereco endereco;
+    public String email;
+    public Timestamp dataCadastro;
 }
