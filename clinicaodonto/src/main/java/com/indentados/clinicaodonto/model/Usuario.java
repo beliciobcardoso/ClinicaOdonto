2c9256e34d02ac42042bec1d52357d94a4b74d6e
Belicio Cardoso <belloinfo@gmail.com>
2022-09-01 20:54:58 -0300

    Adicionado as anotações spring boot

--- clinicaodonto/src/main/java/com/indentados/clinicaodonto/model/Usuario.java
+++ clinicaodonto/src/main/java/com/indentados/clinicaodonto/model/Usuario.java
@@ -1,4 +1,16 @@
 package com.indentados.clinicaodonto.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+    public Timestamp dataCadastro;
 }
