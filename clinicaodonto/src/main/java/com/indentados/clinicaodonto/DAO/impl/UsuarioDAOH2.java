cdae4a88b1c62e636f370f77b7eeedaf26823c72
Belicio Cardoso <belloinfo@gmail.com>
2022-09-02 00:04:20 -0300

    Revisado e refatorado a class UsuarioDAOH2 e corrigido o metodo criarObjetoUsuario

--- clinicaodonto/src/main/java/com/indentados/clinicaodonto/DAO/impl/UsuarioDAOH2.java
+++ clinicaodonto/src/main/java/com/indentados/clinicaodonto/DAO/impl/UsuarioDAOH2.java
@@ -21,33 +21,32 @@ public class UsuarioDAOH2 implements IDao<Usuario> {
     private ConfiguracaoJDBC configuracaoJDBC;
 
     @Override
-    public Usuario salvar(Usuario usuario) throws SQLException {
+    public Usuario salvar(Usuario usuario) {
         logger.info("Cadastrando o Usuario: " + usuario.getNomeDeUsuario());
         configuracaoJDBC = new ConfiguracaoJDBC(jdbcDriver, dbUrl,user,password);
-        Connection connection = configuracaoJDBC.getConnection();
 
-        String query = String.format("INSERT INTO usuario (nomeDeUsuario, senha)" +
-                " VALUES ('%S', '%S')", usuario.getNomeDeUsuario(), usuario.getSenha());
-        try{
+        try (Connection connection = configuracaoJDBC.getConnection()) {
+            String query = String.format("INSERT INTO usuario (nomeDeUsuario, senha)" +
+                    " VALUES ('%S', '%S')", usuario.getNomeDeUsuario(), usuario.getSenha());
             Statement statement = connection.createStatement();
-            statement.execute(query,Statement.RETURN_GENERATED_KEYS);
+            statement.execute(query, Statement.RETURN_GENERATED_KEYS);
             ResultSet resultSet = statement.getGeneratedKeys();
-            if (resultSet.next()){
+            if (resultSet.next()) {
                 usuario.setId(resultSet.getInt(1));
                 logger.info("Capturando o id do banco: " + resultSet.getInt(1));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.info("Ocorreu um erro na aplicação!!!!!");
             e.printStackTrace();
-        }finally {
+        } finally {
             logger.info("Logout do banco efetuado com sucesso!!!!");
-            connection.close();
         }
         return usuario;
     }
 
     @Override
     public List<Usuario> buscarTodos() throws SQLException {
+        logger.info("Buscando todos os Usuarios da tabela usuario");
         configuracaoJDBC = new ConfiguracaoJDBC(jdbcDriver,dbUrl,user,password);
         Connection connection = configuracaoJDBC.getConnection();
 
@@ -58,8 +57,7 @@ public class UsuarioDAOH2 implements IDao<Usuario> {
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery(query);
             while (result.next()){
-                usuarios.add(criarObjetoProduto(result));
-                //erro de difitação, quem será que vai encontrar - Belicio Cardoso
+                usuarios.add(criarObjetoUsuario(result));
             }
         }catch (Exception e){
             logger.info("Ocorreu um erro na aplicação!!!!!");
@@ -77,7 +75,7 @@ public class UsuarioDAOH2 implements IDao<Usuario> {
         configuracaoJDBC = new ConfiguracaoJDBC(jdbcDriver, dbUrl, user, password);
         Connection connection = configuracaoJDBC.getConnection();
         String query = String.format("DELETE FROM usuario WHERE id = '%S'", id);
-        int resultquery = 0;
+        int resultquery;
         boolean estatus = false;
         try {
             Statement statement = connection.createStatement();
@@ -100,62 +98,54 @@ public class UsuarioDAOH2 implements IDao<Usuario> {
     }
 
     @Override
-    public Usuario buscarId(Integer id) throws SQLException {
+    public Usuario buscarId(Integer id)  {
         Usuario usuario = null;
         logger.info("Buscar o Usuario pelo id: " + id);
         configuracaoJDBC = new ConfiguracaoJDBC(jdbcDriver, dbUrl,user,password);
-        Connection connection = configuracaoJDBC.getConnection();
 
-        String query = String.format("SELECT * FROM usuario WHERE ID = %s",id);
-
-        try {
+        try (Connection connection = configuracaoJDBC.getConnection()) {
+            String query = String.format("SELECT * FROM usuario WHERE ID = %s", id);
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(query);
-            while (resultSet.next()){
-                usuario = criarObjetoProduto(resultSet);
-                //colocado de proprosito para ver quem ira nota - Belicio Cardoso
+            while (resultSet.next()) {
+                usuario = criarObjetoUsuario(resultSet);
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.info("Ocorreu um erro na aplicação!!!!!");
             e.printStackTrace();
-        }finally {
+        } finally {
             logger.info("Logout do banco efetuado com sucesso!!!!");
-            connection.close();
         }
         return usuario;
     }
 
     @Override
-    public Usuario atualizar(Usuario usuario) throws SQLException {
+    public Usuario atualizar(Usuario usuario)  {
         logger.info("Alterar o Usuario: " + usuario.getNomeDeUsuario());
         configuracaoJDBC = new ConfiguracaoJDBC(jdbcDriver, dbUrl,user,password);
-        Connection connection = configuracaoJDBC.getConnection();
 
-        String queryUpdate = String.format("UPDATE usuario SET senha = '%S' WHERE id = '%S'",
-                usuario.getSenha(),usuario.getId());
-
-        int resultquery = 0;
-        try {
+        try (Connection connection = configuracaoJDBC.getConnection()) {
+            String queryUpdate = String.format("UPDATE usuario SET senha = '%S' WHERE id = '%S'",
+                    usuario.getSenha(), usuario.getId());
+            int resultquery;
             Statement statement = connection.createStatement();
             resultquery = statement.executeUpdate(queryUpdate);
             logger.info(resultquery);
-            if (resultquery == 1){
+            if (resultquery == 1) {
                 usuario = buscarId(usuario.getId());
-            }else {
+            } else {
                 logger.info("Falhar ao altera o produto");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             logger.info("Ocorreu um erro na aplicação!!!!!");
             e.printStackTrace();
-        }finally {
+        } finally {
             logger.info("Logout do banco efetuado com sucesso!!!!");
-            connection.close();
         }
         return usuario;
     }
 
-    private Usuario criarObjetoProduto(ResultSet resultSet) throws SQLException {
-        //colocado de proprosito para ver quem ira nota - Belicio Cardoso
+    private Usuario criarObjetoUsuario(ResultSet resultSet) throws SQLException {
         Integer id = resultSet.getInt("id");
         String nomeDeUsuario = resultSet.getNString("nomeDeUsuario");
         String senha = resultSet.getNString("senha");
