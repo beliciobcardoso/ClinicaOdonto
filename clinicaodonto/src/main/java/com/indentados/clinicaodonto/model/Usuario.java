477dddb6db52abed92f901c6c379514ff619226c
Belicio Cardoso <belloinfo@gmail.com>
2022-09-01 21:48:47 -0300

    Adicionado as anotações spring boot

--- clinicaodonto/src/main/java/com/indentados/clinicaodonto/model/Usuario.java
+++ clinicaodonto/src/main/java/com/indentados/clinicaodonto/model/Usuario.java
@@ -1,4 +1,13 @@
 package com.indentados.clinicaodonto.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
 }
