32b768f8fda4545c5b0cacaa8887c8648ed49425
rbsebold <89108421+rbsebold@users.noreply.github.com>
2022-09-07 23:16:44 -0300

    atributo sobrenome

--- BackEnd/src/main/java/com/indentados/clinicaodonto/model/Pessoa.java
+++ BackEnd/src/main/java/com/indentados/clinicaodonto/model/Pessoa.java
@@ -14,7 +14,7 @@ import java.sql.Timestamp;
 public class Pessoa {
     public Integer id;
     public String nome;
-    public String sobreNome;
+    public String sobrenome;
     public Endereco endereco;
     public String email;
     public Timestamp dataCadastro;
