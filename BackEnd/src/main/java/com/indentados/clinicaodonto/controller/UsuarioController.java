adc709fc8573be0ab59c30abeabec36e645a5a5d
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-09-08 06:41:19 -0300

    Finalizado a estrutura Usuario e testado com BD

--- BackEnd/src/main/java/com/indentados/clinicaodonto/controller/UsuarioController.java
+++ BackEnd/src/main/java/com/indentados/clinicaodonto/controller/UsuarioController.java
@@ -1,4 +1,28 @@
 package com.indentados.clinicaodonto.controller;
 
+import com.indentados.clinicaodonto.model.Usuario;
+import com.indentados.clinicaodonto.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("usuario")
+@CrossOrigin( "*" )
 public class UsuarioController {
+
+    @Autowired
+    UsuarioService service;
+
+    @PostMapping
+    public ResponseEntity salvar(@RequestBody Usuario usuario){
+        return new ResponseEntity(service.salvar(usuario), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity buscarTodoso(){
+        return  new ResponseEntity(service.buscarTodos(), HttpStatus.OK);
+    }
+
 }
--- BackEnd/src/main/java/com/indentados/clinicaodonto/service/UsuarioService.java
+++ BackEnd/src/main/java/com/indentados/clinicaodonto/service/UsuarioService.java
@@ -1,4 +1,27 @@
 package com.indentados.clinicaodonto.service;
 
+import com.indentados.clinicaodonto.model.Usuario;
+import com.indentados.clinicaodonto.repository.UsuarioRepository;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UsuarioService {
+    final static Logger logger = Logger.getLogger(UsuarioService.class);
+    @Autowired
+    UsuarioRepository repository;
+
+    public Usuario salvar(Usuario usuario){
+        return repository.save(usuario);
+    }
+    public List<Usuario> buscarTodos(){
+        return repository.findAll();
+    }
+
+    public Usuario alterar(Usuario usuario){
+        return repository.save(usuario);
+    }
 }
