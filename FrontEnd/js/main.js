986119cb1e43ffd471d052c3810ec2c44a8b41ad
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-09-11 18:15:41 -0300

    Criado o modal e o formulario do Cadastro de Paciente

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,7 +1,15 @@
-function modal() {
-    document.querySelector(".modal-overlay")
+function modalD() {
+    document.querySelector("#dentista")
         .classList.toggle("active")
         ,            // limpar fomulario
         Form.clearFlields();
+}
+
+function modalP() {
+    document.querySelector("#paciente")
+        .classList.toggle("active")
+        ,            // limpar fomulario
+        Form.clearFlields();
+
 }
 
