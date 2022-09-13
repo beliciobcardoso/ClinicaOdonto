663e40b2435689c3b22f368553d946528cba341c
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-09-13 05:44:09 -0300

    adicionado o @CrossOrigin( "*" )

--- BackEnd/src/main/java/com/indentados/clinicaodonto/controller/ConsultaController.java
+++ BackEnd/src/main/java/com/indentados/clinicaodonto/controller/ConsultaController.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/consulta")
-
+@CrossOrigin( "*" )
 public class ConsultaController {
 
     @Autowired
--- BackEnd/src/main/java/com/indentados/clinicaodonto/controller/EnderecoController.java
+++ BackEnd/src/main/java/com/indentados/clinicaodonto/controller/EnderecoController.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/endereco")
+@CrossOrigin( "*" )
 public class EnderecoController {
 
     @Autowired
