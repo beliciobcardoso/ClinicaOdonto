56d694d2862f4d7e0b59c36b76b97b6537ddaffc
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-09-24 12:40:30 -0300

    Adicionado as Exception e os logs

--- BackEnd/src/main/java/com/indentados/clinicaodonto/service/UsuarioService.java
+++ BackEnd/src/main/java/com/indentados/clinicaodonto/service/UsuarioService.java
@@ -1,7 +1,9 @@
 package com.indentados.clinicaodonto.service;
 
+import com.indentados.clinicaodonto.exception.ResourceNotFoundException;
 import com.indentados.clinicaodonto.model.Usuario;
 import com.indentados.clinicaodonto.repository.UsuarioRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,33 +13,41 @@ import java.util.Optional;
 
 @Service
 public class UsuarioService {
+    final static Logger logger = Logger.getLogger(UsuarioService.class);
     BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
     @Autowired
-    UsuarioRepository repository;
+    UsuarioRepository usuarioRepository;
 
     public Usuario salvar(Usuario usuario){
+        logger.info("Salvador usuario: " + usuario.getUsername());
 
         usuario.setPassword(encoder.encode(usuario.getPassword()));
 
-        return repository.save(usuario);
+        return usuarioRepository.save(usuario);
     }
     public List<Usuario> buscarTodos(){
 
-        return repository.findAll();
+        return usuarioRepository.findAll();
     }
 
     public Optional<Usuario> buscarById(Long id){
-        return repository.findById(id);
+        return usuarioRepository.findById(id);
     }
 
     public Usuario alterar(Usuario usuario){
         usuario.setPassword(encoder.encode(usuario.getPassword()));
 
-        return repository.save(usuario);
+        return usuarioRepository.save(usuario);
     }
 
-    public void delete(Long id){
+    public void delete(Long id) throws ResourceNotFoundException {
 
-        repository.deleteById(id);
-    }
+        try {
+            usuarioRepository.deleteById(id);
+       }catch (Exception exception){
+            logger.info("Erro exception: " + new ResourceNotFoundException("Erro ao excluir usuario, o id informado não existe.", exception));
+           throw new ResourceNotFoundException("Erro ao excluir usuario, o id informado não existe.", exception);
+       }
+
+     }
 }
