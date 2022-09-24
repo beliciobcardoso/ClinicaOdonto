7a22572a0180bd027af6a826952dfd27480bcb12
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-09-24 00:46:45 -0300

    Adicionado o Logger4j

--- BackEnd/src/main/java/com/indentados/clinicaodonto/controller/AuthController.java
+++ BackEnd/src/main/java/com/indentados/clinicaodonto/controller/AuthController.java
@@ -3,6 +3,9 @@ package com.indentados.clinicaodonto.controller;
 import com.indentados.clinicaodonto.DTO.TokenDTO;
 import com.indentados.clinicaodonto.DTO.UsuarioDTO;
 import com.indentados.clinicaodonto.config.security.TokenService;
+import com.indentados.clinicaodonto.exception.ResourceNotFoundException;
+import com.indentados.clinicaodonto.service.AutenticacaoService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +24,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    final static Logger logger = Logger.getLogger(AuthController.class);
     @Autowired
     private AuthenticationManager authManager;
 
@@ -27,7 +32,7 @@ public class AuthController {
     private TokenService tokenService;
 
     @PostMapping
-    public ResponseEntity autenticar(@RequestBody @Valid UsuarioDTO usuarioDTO){
+    public ResponseEntity autenticar(@RequestBody @Valid UsuarioDTO usuarioDTO) throws ResourceNotFoundException {
 
         try {
             UsernamePasswordAuthenticationToken loginUsuario = usuarioDTO.converter();
@@ -42,7 +47,9 @@ public class AuthController {
 
             return new ResponseEntity(tokenDTO, HttpStatus.OK);
         }catch (AuthenticationException exception){
+            logger.info("Erro ao autenticar");
             return new ResponseEntity("Erro ao autenticar",HttpStatus.BAD_REQUEST);
+
         }
 
     }
