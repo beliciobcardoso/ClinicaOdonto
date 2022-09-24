8d84081e7ed5f12da50312b5d5835e8f81c4b94f
Belicio Batista Cardoso <belloinfo@gmail.com>
2022-09-24 00:47:57 -0300

    Adicionado o Logger4j

--- BackEnd/src/main/java/com/indentados/clinicaodonto/service/AutenticacaoService.java
+++ BackEnd/src/main/java/com/indentados/clinicaodonto/service/AutenticacaoService.java
@@ -1,7 +1,9 @@
 package com.indentados.clinicaodonto.service;
 
+import ch.qos.logback.core.joran.conditional.IfAction;
 import com.indentados.clinicaodonto.model.Usuario;
 import com.indentados.clinicaodonto.repository.UsuarioRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AutenticacaoService implements UserDetailsService {
+    final static Logger logger = Logger.getLogger(AutenticacaoService.class);
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -17,8 +20,12 @@ public class AutenticacaoService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
+        logger.info("Recebendo usuario: " + username);
         try {
             Usuario usuario = usuarioRepository.findByUsername(username);
+            logger.info("Resultado da busca do usuario do BD: " + usuario);
+            logger.info("Usuario do banco: " + usuario.getUsername());
+
             return usuario;
         }catch (UsernameNotFoundException exception){
             throw new UsernameNotFoundException("Usuario não existe");
